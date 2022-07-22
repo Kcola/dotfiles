3d98006da19d2fcb1b9486a737881f03f2d60656
Kola Campbell <kolacampbell@microsoft.com>
2022-07-21 21:17:02 -0400

    csharp lsp

---
+++ .config/nvim/lua/kola/lsp/servers/omnisharp.lua
@@ -0,0 +1,17 @@
+local set_keymaps = require("kola.lsp.keymaps").set_keymaps
+local capabilities = require("kola.lsp.config").get_capabilities()
+local register_autocommands = require("kola.lsp.config").register_autocommands
+
+require("lspconfig").omnisharp.setup({
+	handlers = {
+		["textDocument/definition"] = require("omnisharp_extended").handler,
+	},
+	capabilities = capabilities,
+	on_attach = function(client)
+		if client.name == "omnisharp" then
+			client.resolved_capabilities.document_formatting = false
+		end
+		set_keymaps()
+		register_autocommands()
+	end,
+})
