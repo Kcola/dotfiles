2bdfc1c1cea3d8ed5d0e160167606e8a9a3af3bb
Kola Campbell <kolacampbell@microsoft.com>
2023-02-02 23:17:27 -0500

    updates

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -8,5 +8,12 @@ Invoke-Expression (&starship init powershell)
 Set-Alias -Name s -Value save
 Set-Alias -Name g -Value goto
 
+function Get-GitWorktree { & git worktree $args }
+
+New-Alias -Name gw -Value Get-GitWorktree
+
+function Get-GitStatus { & git status $args }
+New-Alias -Name gs -Value Get-GitStatus
+
 Import-Module PSReadLine
 Import-Module z
