d52ead21960247c06a54077bd1a8cf4bb7118cfe
Kola Campbell <kolacampbell@microsoft.com>
2024-02-19 21:46:08 -0800

    winget config

--- init.ps1
+++ init.ps1
@@ -1,25 +1,8 @@
-$DotfilesLocation = "$Home\dotfiles"
- 
-New-Item -ItemType SymbolicLink -Path "$Home/AppData/Local/nvim" -Target "$Home/dotfiles/.config/nvim"
-New-Item -ItemType SymbolicLink -Path "$Home/AppData/Local/diffview" -Target "$Home/dotfiles/.config/diffview"
-echo "created nvim config symlink"
+$PowershellProfileDirectory = "$HOME\Documents\pwsh"
 
-"$PROFILE = 'C:\Users\kolacampbell\dotfiles\.config\powershell\profile.ps1'
-. $PROFILE" | Out-File -FilePath "$HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Encoding UTF8
-echo "updated powershell profile"
-
-if (!(Get-Command git -ErrorAction SilentlyContinue)) {
-    Write-Output "Git is not installed. Installing now..."
-    Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.30.2.windows.2/Git-2.30.2-64-bit.exe -OutFile git.exe
-    Start-Process -FilePath .\git.exe -ArgumentList '/VERYSILENT /DIR=C:\Git' -Wait
-    $env:PATH = [System.Environment]::GetEnvironmentVariable("PATH", "Machine") + ";C:\Git\cmd"
-    [System.Environment]::SetEnvironmentVariable("PATH", $env:PATH, "Machine")
-    Write-Output "Git installed successfully."
-} else {
-    Write-Output "Git is already installed."
+if (-not (Test-Path $PowershellProfileDirectory)) {
+    New-Item -ItemType Directory -Path $PowershellProfileDirectory
 }
 
-git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
-echo "installed packer nvim"
-
-[Environment]::SetEnvironmentVariable("WEZTERM_CONFIG_FILE", "$DotfilesLocation\.config\wezterm\wezterm.lua", "Machine")
+"`$PROFILE = '$HOME\dotfiles\.config\powershell\profile.ps1'
+. `$PROFILE" | Out-File -FilePath "$HOME\Documents\pwsh\Microsoft.PowerShell_profile.ps1" -Encoding UTF8
