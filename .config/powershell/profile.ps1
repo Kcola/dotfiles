bf2b18a56721c30b736be2b8105209f3050ed1b1
Kola Campbell <kolacampbell@microsoft.com>
2024-09-24 00:28:26 -0700

    tmux windows

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -74,5 +74,5 @@ function start-cdb{
  & 'C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe' /port=11000 /NoExplorer
 }
 
-fnm env --use-on-cd | Out-String | Invoke-Expression
+fnm env --use-on-cd | Out-String | Invoke-Expression | Out-Null
 
